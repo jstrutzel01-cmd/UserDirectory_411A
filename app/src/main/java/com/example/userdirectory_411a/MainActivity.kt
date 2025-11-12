256736b23123bd0b432fc19b3420d29f7ba7019a
Joshua Strutzel <jstrutzel01@Joshuas-MacBook-Air-5.local>
2025-11-12 13:27:48 -0800

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/example/userdirectory_411a/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.userdirectory_411a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.userdirectory_411a.ui.theme.UserDirectory_411ATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UserDirectory_411ATheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UserDirectory_411ATheme {
+        Greeting("Android")
+    }
+}
